13446892d5ee028e198d2e08e1302f7411864aaf
JayceFR <jaycejefferson.vicious@gmail.com>
2025-09-23 23:25:22 +0100

    Implemented a simple DNA and FingerPrint

---
+++ src/main/kotlin/FileFingerPrint.kt
@@ -0,0 +1,6 @@
+package org.example
+
+data class FileFingerPrint(
+    val fileName : String,
+    val size : Long,
+)
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,7 @@ package org.example
 import java.util.zip.ZipEntry
 import java.util.zip.ZipFile
 
+typealias DNA = MutableList<FileFingerPrint>
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
@@ -12,8 +13,19 @@ fun main() {
     val name : String = zipFile.name.substringBefore(".zip").substringAfterLast("\\")
     println("Opened ${name}")
     val iterator : Iterator<ZipEntry> = zipFile.entries().asIterator()
+    val fileDNA : DNA = mutableListOf()
     while (iterator.hasNext()){
         val entry : ZipEntry = iterator.next()
-        println(entry.name)
+        fileDNA.add(
+            FileFingerPrint(
+                entry.name,
+                entry.size
+            )
+        )
     }
+    fileDNA.println()
+}
+
+fun DNA.println() : Unit{
+    println(this.joinToString("\n"))
 }
